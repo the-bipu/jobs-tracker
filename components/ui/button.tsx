3ca774002e1fb50841b2952cfd0a436ca27ab9a8
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-11-26 21:52:03 +0530

    Added Components #5

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -20,6 +20,10 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        yellowish:
+          "bg-yellow-400 text-yellow-foreground shadow-xs hover:bg-yellow-300",
+        greenry:
+          "bg-green-800 text-white shadow-xs hover:bg-green-700",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
