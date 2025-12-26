94ff50b57d6c78519477c48daf728ea7dd6294da
The Bipu <127449591+the-bipu@users.noreply.github.com>
2025-12-26 16:03:01 +0530

    Added company data

---
+++ components/assets/food-processing-shark-tank.ts
@@ -0,0 +1,464 @@
+export const sharkTankFood = [
+  {
+    "name": "Munchilicious",
+    "description": "perfect choice to satisfy morning, mid-meal, and late night cravings",
+    "deal_status": "Sponsored",
+    "categories": [],
+    "season": null,
+    "links": {
+      "amazon_store": "https://amzn.to/4isUHoa",
+      "website": "https://munchilicious.com/?ref=MTSRD"
+    }
+  },
+  {
+    "name": "Alpino",
+    "description": "Roasted Peanut Products",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S1",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Alpino%C2%AE/page/C01D82C8-BC1F-459A-9BB7-F883A0EF7E16?&linkCode=sl2&tag=d2cdirectory-21&linkId=3d6b15c0b164d86d938b2e234e72dbb8&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://alpino.store/discount/AMITSARDA?ref=caewqzj8"
+    }
+  },
+  {
+    "name": "Alt Co",
+    "description": "Plant-based dairy products",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Altco/page/B9118AE1-0299-411E-AE5B-9DF61271E70F?&linkCode=sl2&tag=d2cdirectory-21&linkId=fc61a7408fc50df3cf69c3aee91e16fa&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://alt.company"
+    }
+  },
+  {
+    "name": "Barosi",
+    "description": "Organic Milk Products",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Barosi/page/5079B167-B779-4EA6-B326-5542800EE791?&linkCode=sl2&tag=d2cdirectory-21&linkId=5717076f6a04b6a118fcb1d5765c1104&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://barosi.in"
+    }
+  },
+  {
+    "name": "Better Nutrition",
+    "description": "Bio fortified food company",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4b3aTKC",
+      "website": "https://betternutritionforall.in"
+    }
+  },
+  {
+    "name": "Confect",
+    "description": "Vegan Bakery Products",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4hHFNKu",
+      "website": "https://confect.in"
+    }
+  },
+  {
+    "name": "EatBetter",
+    "description": "Millet based snacking brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/3QDaVzB",
+      "website": "https://eatbetterco.com/"
+    }
+  },
+  {
+    "name": "Farmdidi",
+    "description": "Food Business training App",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/page/4B84EFF8-7A8D-4FBD-BD3E-25990137B489?&linkCode=sl2&tag=d2cdirectory-21&linkId=5182654f89f1679a36ae00ed826e6c0d&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://farmdidi.com"
+    }
+  },
+  {
+    "name": "First Bud Organics",
+    "description": "Organic Products",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/page/24336C31-6D9C-43EB-82FA-28FB622F63CC?_encoding=UTF8&store_ref=SB_A05619852QJRN8S0TM0PV-A04551265L1MHTCLDCO6&pd_rd_plhdr=t&aaxitk=0088acbe139295b2f01f120a3ae71691&hsa_cr_id=0&lp_asins=B07Y5DNMS7&lp_query=First+Bud+Organics&lp_slot=auto-sparkle-hsa-tetris&pd_rd_w=MT2rm&content-id=amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d:amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_p=df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_r=M2DQN9DQJZ26HJQYM32Q&pd_rd_wg=aaVDA&pd_rd_r=d111a863-3a20-4e24-b679-11a036d1b25c&linkCode=sl2&tag=d2cdirectory-21&linkId=4ad3fa6ee2a19a24d84c7ae0863486c9&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://firstbud.in"
+    }
+  },
+  {
+    "name": "Fit n Flex",
+    "description": "Healthy Snacking and Cereal Brand",
+    "deal_status": "No Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/page/5C8B426A-D60C-43F8-B1D3-8AA8C4F68B82?_encoding=UTF8&pd_rd_plhdr=t&pd_rd_i=B08PZ3VNTK&store_ref=SBV_A0370547P52SP2WU928F&lp_asins=B08PZ3VNTK,B0BG5KF1NF,B09W66K4YZ&pd_rd_w=NYaBc&content-id=amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d:amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_p=df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_r=VSPSS4CHMG88ATSM7M3Z&pd_rd_wg=ixYwZ&pd_rd_r=481169a3-4ea8-4adb-9cf0-fd37befecc6e&linkCode=sl2&tag=d2cdirectory-21&linkId=80caa52825aac2400ed66330f24b617d&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://fitandflex.in"
+    }
+  },
+  {
+    "name": "FitFeast",
+    "description": "Tasty protein brand",
+    "deal_status": "Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4he17qx",
+      "website": "https://fitfeast.in/"
+    }
+  },
+  {
+    "name": "Glow Glossary",
+    "description": "Matcha tea brand",
+    "deal_status": "Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/3DtPtdc",
+      "website": "https://glowglossary.com/"
+    }
+  },
+  {
+    "name": "Go Zero",
+    "description": "Zero Sugar Icecream brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "",
+      "website": "https://letsgozero.in"
+    }
+  },
+  {
+    "name": "Green Snack",
+    "description": "Healthy Snacks",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/TheGreenSnackCo/Homepage/page/A05C275F-2B71-4043-B57F-AB3C99B62B74?&linkCode=sl2&tag=d2cdirectory-21&linkId=a646cd51513d113e75d74c2ee471af36&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://thegreensnackco.com"
+    }
+  },
+  {
+    "name": "Gud World",
+    "description": "Hygienic Jaggery Brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4aPWBNh",
+      "website": "https://gudworld.in"
+    }
+  },
+  {
+    "name": "Healthy Master",
+    "description": "Healthy munching brand",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/HealthyMaster/page/95E6CFED-2B15-443B-A720-9F416E11EC27?&linkCode=sl2&tag=d2cdirectory-21&linkId=58e65b1d48b855592a9a2d9c3aa726ff&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://healthymaster.in/?ref=d2cdirectory"
+    }
+  },
+  {
+    "name": "HoneyTwigs",
+    "description": "Honey Brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/HONEYTWIGS/page/6D4FA586-4317-48F6-B457-9B0DA0DE49A3?&linkCode=sl2&tag=d2cdirectory-21&linkId=241a03f84e98e12671c0cbee60fceb08&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://honeytwigs.co"
+    }
+  },
+  {
+    "name": "HoneyVeda",
+    "description": "Honey Products",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/HoneyVeda/Ourproducts/page/0EA619D0-7ECB-4B64-A921-E9C17E446627?&linkCode=sl2&tag=d2cdirectory-21&linkId=b6f7a14a53d89741ef9eef8fda0785e9&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://honeyveda.in"
+    }
+  },
+  {
+    "name": "Jain Shikanji",
+    "description": "Lemonade",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S1",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/JainShikanji/page/E4185975-6CE4-4B2B-B36A-47BE848AD870?&linkCode=sl2&tag=d2cdirectory-21&linkId=94f133c563efa1240dc326e15efa25cb&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://jainshikanjimasala.com"
+    }
+  },
+  {
+    "name": "JoySpoon",
+    "description": "Health Mouth freshner Brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/3CHrTJL",
+      "website": "https://joyspoon.in"
+    }
+  },
+  {
+    "name": "KIWI Kisan Window",
+    "description": "One stop solution for experiencing unique flavors of India",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4hhgvTR",
+      "website": "https://kiwikisanwindow.com"
+    }
+  },
+  {
+    "name": "MadMix",
+    "description": "Baked Snacks",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4hUjKRC",
+      "website": "https://eatmadmix.com"
+    }
+  },
+  {
+    "name": "Makino",
+    "description": "Nachos and nuts",
+    "deal_status": "No Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Makino/page/7AE2E859-BBA5-4DAB-8E06-1BA7503489B2?&linkCode=sl2&tag=d2cdirectory-21&linkId=8bf89b15b146ff0ea91fdd0cdaf726e0&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://makinofoods.in"
+    }
+  },
+  {
+    "name": "Mavis",
+    "description": "Vegan Fermented Food",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S1",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/MAVIS/page/3B91472D-54D5-4B57-A26A-06ACF6580151?&linkCode=sl2&tag=d2cdirectory-21&linkId=31a0918c066de4764b695ede18a95af0&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://mavispantry.com"
+    }
+  },
+  {
+    "name": "Millet Amma",
+    "description": "Millet based Products",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/MilletAmma/MilletAmma/page/D666C51F-994C-4386-B01E-F55B12CE2079?&linkCode=sl2&tag=d2cdirectory-21&linkId=8bf36cbbed0cdac97f5a2a49098c1656&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://milletamma.com"
+    }
+  },
+  {
+    "name": "Namakwali",
+    "description": "Organic Spices",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food", "Ayurveda"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Namakwali/page/CE278223-BE46-49AF-B816-975F6C1F62D8?store_ref=bl_ast_dp_brandLogo_sto&linkCode=sl2&tag=d2cdirectory-21&linkId=3848612e7e95a63ba5a34efe2f67ebca&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://namakwali.com"
+    }
+  },
+  {
+    "name": "Namhya Foods",
+    "description": "Ayurvedic Enriched Food",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S1",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Namhya/Homepage/page/80BAD4E7-C842-4872-9381-689A3383F7F8?&linkCode=sl2&tag=d2cdirectory-21&linkId=cf184c8a4d7fba194f0d03f173a02b8d&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://namhyafoods.com"
+    }
+  },
+  {
+    "name": "Nomad Food Project",
+    "description": "Bacon Jams",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S1",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/NomadFoodProject/page/11ABD2EC-076D-4D03-AA67-45E4BA81D45F?&linkCode=sl2&tag=d2cdirectory-21&linkId=fbb3bc041a9ea16737c5655d1c8507ea&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://nomadfoodproject.com"
+    }
+  },
+  {
+    "name": "Panchal Dairy",
+    "description": "Goat and sheep cheese brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "",
+      "website": "https://www.panchaldairy.com/"
+    }
+  },
+  {
+    "name": "Popcorn and Company",
+    "description": "Ready to Eat Popcorn",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/PopcornCompany/page/E1C97FDA-E799-4161-A785-CF26D1273F87?store_ref=bl_ast_dp_brandLogo_sto&linkCode=sl2&tag=d2cdirectory-21&linkId=be7462f20849990bafab02c2a8ba7315&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://pncpopcorn.com"
+    }
+  },
+  {
+    "name": "Raheja Solar Food Processing",
+    "description": "Food processing equipment manufacturers",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/42M9GoH",
+      "website": "https://www.rsfp.in/"
+    }
+  },
+  {
+    "name": "Repeat Gud",
+    "description": "Jaggery based food Brand",
+    "deal_status": "Deal",
+    "categories": ["Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/3WSMqSn",
+      "website": "https://repeatgud.com/"
+    }
+  },
+  {
+    "name": "Rocca",
+    "description": "Premium Chocolate Brand",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/ROCCATheNewAgeChocolate/page/E55B9EF4-169C-434B-9E02-1D97635CFA80?&linkCode=sl2&tag=d2cdirectory-21&linkId=7cedf7de4e53035d751fec4e0933c9fa&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://myrocca.in"
+    }
+  },
+  {
+    "name": "SORICH",
+    "description": "Guilt free Snacks",
+    "deal_status": "No Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/page/447AC4FC-9D27-44D6-A2B7-51BED9B1472D?&linkCode=sl2&tag=d2cdirectory-21&linkId=03af772c5b74edc86cb565689502ae10&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://sorichorganics.com/?ref=d2cdirectory"
+    }
+  },
+  {
+    "name": "The Cinnamon Kitchen",
+    "description": "Plant-based healthy snacks",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/TheCinnamonKitchen/page/5FD45B54-94E3-4C48-B450-F552809E9C0E?&linkCode=sl2&tag=d2cdirectory-21&linkId=92362629717498ddd94a8d3e2c0a6bfd&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://cinnamon.kitchen"
+    }
+  },
+  {
+    "name": "The Health Factory",
+    "description": "Healthy protein bread",
+    "deal_status": "No Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/TheHealthFactory/page/973D04A2-1F4E-478B-9A44-7CDBE1C9E8E5?&linkCode=sl2&tag=d2cdirectory-21&linkId=c48291b783d3ad2da21efbc75fa34817&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://thehealthfactory.in"
+    }
+  },
+  {
+    "name": "The Healthy Binge",
+    "description": "Healthy Snacks",
+    "deal_status": "Deal",
+    "categories": ["Snacks", "Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Thehealthybinge/page/5318DFD2-2DAD-431D-8A15-046F3CA1CF26?&linkCode=sl2&tag=d2cdirectory-21&linkId=662fd5f73461e202ab0d91f463bb5ee9&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://healthybinge.co.in"
+    }
+  },
+  {
+    "name": "The Naturik co",
+    "description": "Preservative free food brand",
+    "deal_status": "Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4jSiksa",
+      "website": "https://thenaturikco.com/"
+    }
+  },
+  {
+    "name": "Tiggle",
+    "description": "Chocolate mixes",
+    "deal_status": "Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S3",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Tiggle/page/DF2283EA-1EF6-45AF-818C-3DB7B973A69F?&linkCode=sl2&tag=d2cdirectory-21&linkId=805475994c5046e51f9562c015891a45&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://thetiggle.com"
+    }
+  },
+  {
+    "name": "Tribalveda",
+    "description": "Jamun products",
+    "deal_status": "Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/43x7Bxk",
+      "website": "https://tribalveda.in/"
+    }
+  },
+  {
+    "name": "Utopian",
+    "description": "Preservative free Beverages",
+    "deal_status": "No Deal",
+    "categories": ["Beverages", "Healthy Food"],
+    "season": "S4",
+    "links": {
+      "amazon_store": "https://amzn.to/4jRJlvw",
+      "website": "https://utopian.fit/"
+    }
+  },
+  {
+    "name": "Wicked Gud",
+    "description": "Healthy Pasta and Noodles",
+    "deal_status": "No Deal",
+    "categories": ["Healthy Food"],
+    "season": "S2",
+    "links": {
+      "amazon_store": "https://www.amazon.in/stores/Wickedgud/page/7118A80A-BE02-4C70-A13F-3AB3677FAF79?&linkCode=sl2&tag=d2cdirectory-21&linkId=fd789178569639f4a2f82d9e30ef22fb&language=en_IN&ref_=as_li_ss_tl",
+      "website": "https://wickedgud.com"
+    }
+  }
+]
